dd6c362f1a685d952fd68da1ecc4da2d0cd80bf2
Andrew Hsieh <andrew891221@gmail.com>
2023-03-17 18:56:52 +0800

    updated files

--- Depth Camera D415/tensorflow_object_detection.py
+++ Depth Camera D415/tensorflow_object_detection.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 # Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 print("[INFO] Starting streaming...")
 pipeline.start(config)
@@ -21,7 +21,7 @@ PATH_TO_CKPT = "frozen_inference_graph.pb"
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.compat.v1.GraphDef()
-    with tf.compat.v1.gfile.GFile(PATH_TO_CKPT, "rb", encoding="utf-16") as fid:
+    with tf.compat.v1.gfile.GFile(PATH_TO_CKPT, "rb") as fid:
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.compat.v1.import_graph_def(od_graph_def, name="")
@@ -43,49 +43,54 @@ num_detections = detection_graph.get_tensor_by_name("num_detections:0")
 print("[INFO] Model loaded.")
 colors_hash = {}
 
-while True:
-    frames = pipeline.wait_for_frames()
-    color_frame = frames.get_color_frame()
+try:
+    while True:
+        frames = pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
 
-    # Convert images to numpy arrays
-    color_image = np.asanyarray(color_frame.get_data())
-    scaled_size = (color_frame.width, color_frame.height)
-    # expand image dimensions to have shape: [1, None, None, 3]
-    # i.e. a single-column array, where each item in the column has the pixel RGB value
-    image_expanded = np.expand_dims(color_image, axis=0)
-    # Perform the actual detection by running the model with the image as input
-    (boxes, scores, classes, num) = sess.run(
-        [detection_boxes, detection_scores, detection_classes, num_detections],
-        feed_dict={image_tensor: image_expanded},
-    )
+        # Convert images to numpy arrays
+        color_image = np.asanyarray(color_frame.get_data())
+        scaled_size = (color_frame.width, color_frame.height)
+        # expand image dimensions to have shape: [1, None, None, 3]
+        # i.e. a single-column array, where each item in the column has the pixel RGB value
+        image_expanded = np.expand_dims(color_image, axis=0)
+        # Perform the actual detection by running the model with the image as input
+        (boxes, scores, classes, num) = sess.run(
+            [detection_boxes, detection_scores, detection_classes, num_detections],
+            feed_dict={image_tensor: image_expanded},
+        )
 
-    boxes = np.squeeze(boxes)
-    classes = np.squeeze(classes).astype(np.int32)
-    scores = np.squeeze(scores)
+        boxes = np.squeeze(boxes)
+        classes = np.squeeze(classes).astype(np.int32)
+        scores = np.squeeze(scores)
 
-    for idx in range(int(num)):
-        class_ = classes[idx]
-        score = scores[idx]
-        box = boxes[idx]
+        for idx in range(int(num)):
+            class_ = classes[idx]
+            score = scores[idx]
+            box = boxes[idx]
 
-        if class_ not in colors_hash:
-            colors_hash[class_] = tuple(np.random.choice(range(256), size=3))
+            if class_ not in colors_hash:
+                colors_hash[class_] = tuple(np.random.choice(range(256), size=3))
 
-        if score > 0.6:
-            left = int(box[1] * color_frame.width)
-            top = int(box[0] * color_frame.height)
-            right = int(box[3] * color_frame.width)
-            bottom = int(box[2] * color_frame.height)
+            if score > 0.6:
+                left = int(box[1] * color_frame.width)
+                top = int(box[0] * color_frame.height)
+                right = int(box[3] * color_frame.width)
+                bottom = int(box[2] * color_frame.height)
 
-            p1 = (left, top)
-            p2 = (right, bottom)
-            # draw box
-            r, g, b = colors_hash[class_]
-            cv2.rectangle(color_image, p1, p2, (int(r), int(g), int(b)), 2, 1)
+                p1 = (left, top)
+                p2 = (right, bottom)
+                # draw box
+                r, g, b = colors_hash[class_]
+                cv2.rectangle(color_image, p1, p2, (int(r), int(g), int(b)), 2, 1)
 
-    cv2.namedWindow("RealSense", cv2.WINDOW_AUTOSIZE)
-    cv2.imshow("RealSense", color_image)
-    cv2.waitKey(1)
+        cv2.namedWindow("RealSense", cv2.WINDOW_AUTOSIZE)
+        cv2.imshow("RealSense", color_image)
+        key = cv2.waitKey(1)
 
-print("[INFO] stop streaming ...")
-pipeline.stop()
+        if key == 27 or key == ord("q"):  # Esc
+            break
+
+finally:
+    print("[INFO] stop streaming ...")
+    pipeline.stop()
